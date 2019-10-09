4a9cc9d6172adf8e8697c8463e195c56d2b664be
Herman Rivas <hrivas5726@aol.com>
2019-10-09 17:56:33 -0400

    CSS on sidenav

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -15,7 +15,7 @@ class Sidebar extends Component {
 
  render() {
    return (
-     <div>
+     <div >
        <ul
          ref={Sidenav => {
            this.Sidenav = Sidenav;
@@ -23,51 +23,50 @@ class Sidebar extends Component {
          id="slide-out"
          className="sidenav sidenav-fixed"
        >
-         <li>
+         <li >
            <div className="user-view">
              <div className="background">
-               <img className="profile-pic" src={require('../blue_background.jpg')} alt="profile-pic"/>
+
              </div>
              <div className="pics">
              <a href="#user">
-               <img className="circle" src={require('../profile_pic.jpg')} alt="background-pic"/>
+               <img className="circle" src={require('../new_pic.jpg')} alt="background-pic"/>
              </a>
-
-               <strong className="white-text name">Herman Rivas</strong>
-
+               <strong id="name" className="white-text name">Herman Rivas</strong>
              <a href="#email">
-               <span className="white-text email ">Software Engineer</span>
+               <span id="title" className="grey-text email ">Software Engineer</span>
              </a>
              </div>
            </div>
+           <div className="divider"></div>
          </li>
-         <li>
-           <a href="#!">
-             Intro <i className="material-icons right">home</i>
+         <li className="intro">
+           <a id="introduction" href="#!">
+             Intro <i className="material-icons right grey-text">home</i>
            </a>
          </li>
-         <li>
-           <a href="#!">
-            Portfolio <i className="material-icons right">apps</i>
+         <li className="port">
+           <a id="portfolio" href="#!">
+            Portfolio <i className="material-icons right grey-text">apps</i>
+            </a>
+         </li>
+         <li className="ab">
+           <a id="about" href="#!">
+            About Me <i className="material-icons right grey-text">account_box</i>
+            </a>
+         </li>
+         <li className="co">
+           <a id="contact" href="#!">
+            Contact <i className="material-icons right grey-text ">mail_outline</i>
             </a>
          </li>
          <li>
-           <a href="#!">
-            About Me <i className="material-icons right">account_box</i>
-            </a>
-         </li>
-         <li>
-           <a href="#!">
-            Contact <i className="material-icons right ">mail_outline</i>
-            </a>
-         </li>
-         <li>
-           <div className="divider" />
+            <div className="divider"></div>
          </li>
          <div className="social-icons">
-           <a class="waves-effect waves-light btn-floating social linkedin" href="https://www.linkedin.com/in/hermsrivas/"><i class="fa fa-linkedin"></i></a>&nbsp;&nbsp;
-           <a class="waves-effect waves-light btn-floating social-icon github" href="https://github.com/speedy012"><i class="fa fa-github"></i></a>&nbsp;
-           <a class="waves-effect waves-light btn-floating social-icon google" href="mailto:hermsrivas@gmail.com"><i class="fa fa-google"></i></a>
+           <a id="linkedin" className="waves-effect waves-light btn-floating social linkedin " href="https://www.linkedin.com/in/hermsrivas/"><i class="fa fa-linkedin"></i></a>&nbsp;
+           <a id="github" className="waves-effect waves-light btn-floating social-icon github" href="https://github.com/speedy012"><i class="fa fa-github"></i></a>&nbsp;
+           <a id="gmail" className="waves-effect waves-light btn-floating social-icon google" href="mailto:hermsrivas@gmail.com"><i class="fa fa-google"></i></a>
          </div>
        </ul>
        <a href="#!" data-target="slide-out" className="sidenav-trigger">
