af5ab68f8525ba38bc5db5fb342e400338126ce6
Herman Rivas <hrivas5726@aol.com>
2019-10-30 17:13:35 -0400

    modified c-comp

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -17,7 +17,7 @@ class Contact extends React.Component {
       <h3> Contact</h3>
       <h4> If you have any questions about my projects <br/>or will like to connect, <br/> just click on the link to send me an email: <a id="link" href="mailto:hermsrivas@gmail.com?subject=Request Resume"> here!</a></h4>
       <br/>
-      <i onClick={this.scrollIntro} className="large material-icons" href="#!">arrow_upward</i>
+      <i onClick={this.scrollIntro} className="large material-icons " href="#!">arrow_upward</i>
     </div>
 
     </div>
