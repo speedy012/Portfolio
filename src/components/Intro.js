60f53b857bddb057ebbda68e0e1e613c0024a67d
Herman Rivas <hrivas5726@aol.com>
2019-10-31 11:35:37 -0400

    testing

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -11,7 +11,7 @@ class Intro extends React.Component {
       <h2 className="grey-text"> Hi! I'm <span className="highlight"> Herman</span>, a <span className="highlight" >software engineer </span>
         <br/>living and working in <span className="highlight">NYC</span>
       </h2>
-
+      /*test*/
       </div>
     </div>
   )
